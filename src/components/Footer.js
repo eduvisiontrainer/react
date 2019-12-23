0fb4d997b1e3428467d4d939e6d0835cb987a269
eduvisiontrainer <40383284+eduvisiontrainer@users.noreply.github.com>
2019-12-24 00:51:05 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,8 @@ import './Footer.css'
 export default () => (
   <div>
     <h2 className="taCenter">
-      Follow us{' '}
-      <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a>
+      Volg mij{' '}
+      <a href="https://www.instagram.com/authorjimdunk/">@authorjimdunk</a>
     </h2>
     <br />
     <InstagramFeed count="8" />
@@ -14,7 +14,7 @@ export default () => (
       <div className="container taCenter">
         <span>
           © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
-          <a href="https://thriveweb.com.au/">Thrive</a>.
+          <a href="http://unboundit.com/">UnboundiT</a>.
         </span>
       </div>
     </footer>
